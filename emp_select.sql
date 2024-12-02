4b11e36c3ac9b6d26a623da7ee34fd17acbb719c
ryanlukito <ryanlukito13@gmail.com>
2024-12-02 09:32:32 +0700

    fix

--- emp_select.sql
+++ emp_select.sql
@@ -5,14 +5,16 @@ returns table
 	_nama character varying,
 	_id_dept int,
 	_nama_dept character varying,
-	_id_jabatan int
+	_id_jabatan int,
+	_nama_jabatan character varying,
+	_salary numeric
 )
 language plpgsql
 AS
 $$
 	begin
 		return query
-		select k.id_karyawan, k.nama, k.id_dept, d.nama_dept, j.id_jabatan from karyawan k 
+		select k.id_karyawan, k.nama, k.id_dept, d.nama_dept, k.id_jab, j.nama_jabatan, j.salary  from karyawan k 
 		inner join departemen d on k.id_dept = d.id_departemen
 		inner join jabatan j on k.id_jab = j.id_jabatan;
 	end
